7ed0f60dab4df1149f55442dc15b5ca4a5184539
root <root@hujiafu-virtual-machine.(none)>
2016-05-04 14:35:24 +0800

    modified:   pos_realtime.c

--- mg-samples-3.0.12/huge/pos_realtime.c
+++ mg-samples-3.0.12/huge/pos_realtime.c
@@ -6,6 +6,12 @@
 int get_time()
 {
 	time_t timep;
+	struct tm *p;
+
 	time(&timep);
-	printf("%s",asctime(gmtime(&timep)));
+	p=localtime(&timep);
+	printf ("%d%d%d", (1900+p->tm_year),(1 + p->tm_mon), p->tm_mday);
+	printf("%d:%d:%d\n",p->tm_hour, p->tm_min, p->tm_sec);
 }
+
+
