036584f54c0c82d47462820f04e24dc50f663f70
root <root@ubuntu.(none)>
2016-08-11 14:54:20 +0800

    new file:   JointWarn_Wtv.h

---
+++ mg-samples-3.0.12/Joint/JointWarn/JointWarn_Wtv.h
@@ -0,0 +1,17 @@
+#ifndef __JOINTWARN_WTV_H__
+#define __JOINTWARN_WTV_H__
+
+int JointWarn_GetWtv();
+void JointWarn_DelWtv(int fd);
+void JointWarn_WriteWtv(int fd, unsigned char data);
+void JointWarn_OpenWtv(int fd);
+void JointWarn_StopWtv(int fd);
+void JointWarn_CloseWtv(int fd);
+void JointWarn_CloseWtv(int sel);
+
+
+
+
+
+
+#endif
